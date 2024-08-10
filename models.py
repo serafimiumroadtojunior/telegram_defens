05aaedc720f81705e7770715d238c4c55c4f0c67
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-08-10 17:44:46 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,20 +1,21 @@
+import os
 from typing import Annotated
-from sqlalchemy import String, Integer, BigInteger
+
+from sqlalchemy import Integer, BigInteger
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from dotenv import load_dotenv
 
+load_dotenv()
 
-DATABASE_URL = 'YOUR_DATABASE_URL'
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_async_engine(url=DATABASE_URL)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
-
 intpk = Annotated[int, mapped_column(primary_key=True)]
-string = Annotated[str, mapped_column(String(125), nullable=False)]
 warint = Annotated[int, mapped_column(Integer, nullable=False, default=0)]
-
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -23,11 +24,5 @@ class User(Base):
     __tablename__ = 'users_moderations'
     
     id: Mapped[intpk]
-    tg_id: Mapped[int] = mapped_column(BigInteger)
+    tg_id: Mapped[BigInteger] = mapped_column(BigInteger)
     warns: Mapped[warint]
-
-
-async def async_main():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        print("Tables created successfully.")
