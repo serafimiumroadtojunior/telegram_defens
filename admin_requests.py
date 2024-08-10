8a54625fe449c87a3c7d845d17985f4afef34c84
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-08-10 17:42:21 +0300

    Update admin_requests.py

--- admin_requests.py
+++ admin_requests.py
@@ -1,8 +1,13 @@
 from sqlalchemy.future import select
+
 from models import async_session, User
 
-# Функция для добавления предупреждений
 async def add_warn(user_id: int) -> int:
+    """
+    Функция для выдачи варна юзеру по айди
+    :param user_id: Айди юзера
+    :return: Количество варнов у юзера
+    """
     async with async_session() as session:
         async with session.begin():
             result = await session.execute(select(User).where(User.tg_id == user_id))
@@ -10,10 +15,12 @@ async def add_warn(user_id: int) -> int:
             if user:
                 user.warns += 1
             await session.commit()
-            return user.warns
 
-# Функция для сброса предупреждений
 async def reset_warns(user_id: int):
+    """
+    Функция для сброса варнов у юзера по айди
+    :param user_id: Айди юзера
+    """
     async with async_session() as session:
         async with session.begin():
             result = await session.execute(select(User).where(User.tg_id == user_id))
@@ -22,29 +29,49 @@ async def reset_warns(user_id: int):
                 user.warns = 0
             await session.commit()
 
-# Функция для проверки количества предупреждений
 async def check_warns(user_id: int) -> int:
+    """
+    Функция для проверки количества варнов у юзера по айди
+    :param user_id: Айди юзера
+    :return: Количество варнов у юзера
+    """
     async with async_session() as session:
         result = await session.execute(select(User).where(User.tg_id == user_id))
         user = result.scalars().first()
         return user.warns if user else 0
 
-# Функция для добавления нового пользователя
 async def add_user(tg_id: int):
+    """
+    Функция для добавления нового пользователя по айди
+    :param tg_id: Айди юзера
+    """
     async with async_session() as session:
         async with session.begin():
             user = User(tg_id=tg_id)
             session.add(user)
             await session.commit()
-            print(f"User with tg_id {tg_id} created.")
 
-# Проверка существования пользователя
 async def check_user(tg_id: int):
+    """
+    Функция для проверки существования пользователя по айди
+    :param tg_id: Айди юзера
+    """
     async with async_session() as session:
         result = await session.execute(select(User).where(User.tg_id == tg_id))
-        user = result.scalars().first()  # Извлекаем первый результат, если есть
+        user = result.scalars().first()  
 
         if user is None:
-            await add_user(tg_id=tg_id)  # Добавляем пользователя, если не найден
-        else:
-            print(f"User with tg_id {tg_id} already exists.")
+            await add_user(tg_id=tg_id)
+
+async def del_warn(tg_id: int):
+    """
+    Функция для удаления варна у юзера по айди
+    :param tg_id: Айди юзера
+    """
+    async with async_session() as session:
+        async with session.begin():
+            result = await session.execute(select(User).where(User.tg_id == tg_id))
+            user = result.scalars().first()
+            if user.warns > 0:
+                user.warns -= 1
+            await session.commit()
