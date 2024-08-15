7b8387c7f8a5cf07f29b0f04faed39d6a81069c6
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-08-15 18:42:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-from asyncio import Lock
 import os
 
 from aiogram import Bot, Dispatcher, F
@@ -9,20 +8,19 @@ from aiogram.exceptions import TelegramBadRequest
 from dotenv import load_dotenv
 
 from midlewares import (AdminCheckerMiddleware, CallbackAdminCheckerMiddleware,
-                        ForbiddenWordsMiddleware, AntiFloodMiddleware,
-                        AddUserToDataBase)
+                        ForbiddenWordsMiddleware, AntiFloodMiddleware)
+
 from functions import (handle_restriction, handle_unrestriction,
-                       handle_unrestriction_for_callback)
+                       handle_unrestriction_for_callback, ban_user,  delayed_delete)
+
 from models import engine, Base
-from admin_requests import add_warn, reset_warns, check_warns, del_warn
+from admin_requests import add_warn, reset_warns, check_warns, del_warn, add_user
 from other import forbidden_words
 
 load_dotenv()
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
-
-lock = Lock()
 
 @dp.message(Command('mute'))
 async def mute_handler(message: Message, command: CommandObject): 
@@ -64,16 +62,14 @@ async def unban_callback_handler(callback_query: CallbackQuery):
     Отлавливает специальный каллбек по которому понимает что надо удалить варн юзера
     :param callback_query: Объект CallbackQuery
     """
-    async with lock:
-        user_id = int(callback_query.data.split('_')[1])
-        await del_warn(user_id)
+    user_id = int(callback_query.data.split('_')[1])
+    await del_warn(user_id)
 
-        await callback_query.message.delete()
+    await callback_query.message.delete()
 
-        user_info = await bot.get_chat_member(callback_query.message.chat.id, user_id)
-        sent_message = await callback_query.message.answer(f"👀The <a href='tg://user?id={user_info.user.id}'><b>{user_info.user.first_name}</b></a> warn has been removed", parse_mode='HTML')
-        await asyncio.sleep(30)
-        await sent_message.delete()
+    user_info = await bot.get_chat_member(callback_query.message.chat.id, user_id)
+    sent_message = await callback_query.message.answer(f"👀The <a href='tg://user?id={user_info.user.id}'><b>{user_info.user.first_name}</b></a> warn has been removed", parse_mode='HTML')
+    await delayed_delete(30, sent_message)
 
 @dp.message(Command('unmute'))
 async def unmute_handler(message: Message):
@@ -102,12 +98,12 @@ async def warn_user(message: Message, command: CommandObject):
 
     if not reply:
         sent_message = await message.answer("🔴<b>This command must be used as a reply to a user's message.</b>", parse_mode='HTML')
-        await asyncio.sleep(30)
-        await sent_message.delete()
+        await delayed_delete(10, sent_message)
         return
 
     reason = command.args if command.args else "no reason provided"
 
+    await add_user(reply.from_user.id)
     await add_warn(reply.from_user.id)
     warns = await check_warns(reply.from_user.id)
 
@@ -118,36 +114,28 @@ async def warn_user(message: Message, command: CommandObject):
     unban_keyboard = InlineKeyboardMarkup(inline_keyboard=[[unban_button]])
 
     if warns >= 3:
+        await reset_warns(reply.from_user.id)
         try:
-            await reset_warns(reply.from_user.id)
-            await bot.ban_chat_member(
-                chat_id=message.chat.id,
-                user_id=reply.from_user.id
-            )
-
-            sent_message = await message.answer(
-                f"👀<a href='tg://user?id={reply.from_user.id}'><b>{reply.from_user.first_name}</b></a> has been permanently banned for receiving 3 warnings.",
-                reply_markup=unban_keyboard,
-                parse_mode="HTML"
-            )
-
-            await asyncio.sleep(30)
-            await sent_message.delete()
-
+            await ban_user(bot= bot, tg_id= reply.from_user.id, chat_id= message.chat.id)
         except TelegramBadRequest:
-            sent_message = await message.answer(f"<b>🔴Error banning user</b>", parse_mode='HTML')
-            await asyncio.sleep(30)
-            await sent_message.delete()
-            return
-        
+            sent_message = await message.answer("<b>🔴Error ban!</b>", parse_mode='HTML')
+            await delayed_delete(10, sent_message)
+
+        sent_message = await message.answer(
+            f"👀<a href='tg://user?id={reply.from_user.id}'><b>{reply.from_user.first_name}</b></a> has been permanently banned for receiving 3 warnings.",
+            reply_markup=unban_keyboard,
+            parse_mode="HTML"
+        )
+
+        await delayed_delete(30, sent_message)
+   
     else:
         sent_message = await message.answer(
             f"👀<a href='tg://user?id={reply.from_user.id}'><b>{reply.from_user.first_name}</b></a> has received a warning for: {reason}. \n<i>Current count: {warns}.</i>", 
             parse_mode="HTML", 
             reply_markup=warn_keyboard
         )
-        await asyncio.sleep(30)
-        await sent_message.delete()
+        await delayed_delete(30, sent_message)
 
 async def main():
     """
@@ -160,7 +148,6 @@ async def main():
     dp.callback_query.middleware(CallbackAdminCheckerMiddleware(bot))
     dp.message.outer_middleware(ForbiddenWordsMiddleware(bot, forbidden_words))
     dp.message.middleware(AntiFloodMiddleware())
-    dp.message.outer_middleware(AddUserToDataBase())
 
     await dp.start_polling(bot)
 
