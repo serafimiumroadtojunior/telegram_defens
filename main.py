cb7fc2fd73d37eabbb6576058613d2bf9a99b58e
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-08-25 12:23:43 +0300

    new handlers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import re
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandObject
@@ -8,7 +9,7 @@ from aiogram.exceptions import TelegramBadRequest
 from dotenv import load_dotenv
 
 from midlewares import (AdminCheckerMiddleware, CallbackAdminCheckerMiddleware,
-                        ForbiddenWordsMiddleware, AntiFloodMiddleware)
+                        ForbiddenWordsMiddleware, AntiFloodMiddleware, WelcomeMiddleware)
 
 from functions import (handle_restriction, handle_unrestriction,
                        handle_unrestriction_for_callback, ban_user,
@@ -25,44 +26,22 @@ dp = Dispatcher()
 
 @dp.message(Command('mute'))
 async def mute_handler(message: Message, command: CommandObject): 
-    """
-    Эта функция мутит юзера в чате
-    :param message: Объект Message
-    :param command: Объект CommandObject
-    """   
     await handle_restriction(bot, message, command, "mute")
 
 @dp.message(Command('ban'))
 async def ban_handler(message: Message, command: CommandObject):
-    """
-    Эта функция банит юзера в чате
-    :param message: Объект Message
-    :param command: Объект CommandObject
-    """
     await handle_restriction(bot, message, command, "ban")
 
 @dp.callback_query(F.data.startswith('unmute_'))
 async def unmute_callback_handler(callback_query: CallbackQuery):
-    """
-    Отлавливает специальный каллбек по которому понимает что надо размутить юзера
-    :param callback_query: Объект CallbackQuery
-    """
     await handle_unrestriction_for_callback(bot, callback_query, "unmute")
 
 @dp.callback_query(F.data.startswith('unban_'))
 async def unban_callback_handler(callback_query: CallbackQuery):
-    """
-    Отлавливает специальный каллбек по которому понимает что надо разбанить юзера
-    :param callback_query: Объект CallbackQuery
-    """
     await handle_unrestriction_for_callback(bot, callback_query, "unban")
 
 @dp.callback_query(F.data.startswith('rewarn_'))
 async def unban_callback_handler(callback_query: CallbackQuery):
-    """
-    Отлавливает специальный каллбек, по которому понимает, что надо удалить варн юзера.
-    :param callback_query: Объект CallbackQuery.
-    """
     await callback_query.message.delete()
     
     user_id = int(callback_query.data.split('_')[1])
@@ -79,27 +58,14 @@ async def unban_callback_handler(callback_query: CallbackQuery):
 
 @dp.message(Command('unmute'))
 async def unmute_handler(message: Message):
-    """
-    Эта функция размутит юзера в чате
-    :param message: Объект Message
-    """
     await handle_unrestriction(bot, message, "unmute")
 
 @dp.message(Command('unban'))
 async def unban_handler(message: Message):
-    """
-    Эта функция разбанит юзера в чате
-    :param message: Объект Message
-    """
     await handle_unrestriction(bot, message, "unban")
 
 @dp.message(Command("warn"))
 async def warn_user(message: Message, command: CommandObject):
-    """
-    Функция для выдачи варна.
-    :param message: Объект Message.
-    :param command: Объект CommandObject.
-    """
     reply = message.reply_to_message
 
     if not reply:
@@ -151,13 +117,49 @@ async def warn_user(message: Message, command: CommandObject):
             reply_markup=await optional_keyboard('Delete Warn✅',  f'rewarn_{reply.from_user.id}')
         )
 
+@dp.message(Command("message_id"))
+async def get_id(message: Message):
+    reply = message.reply_to_message
+    if not reply:
+        await send_message_and_delete(
+            bot=message.bot,
+            chat_id=message.chat.id,
+            text="🔴<b>This command must be used as a reply to a user's message.</b>",
+            delay=10,
+            parse_mode='HTML'
+        )
+        return
+    
+    replied_message_id = reply.message_id
+    await message.reply(f"Message ID: {replied_message_id}")
+
+@dp.message(Command('set_rules'))
+async def help_handler(message: Message, command: CommandObject):
+    args = command.args.strip()
+
+    if re.fullmatch(r'\d+', args):
+        message_id = int(args)
+        
+        dp.message.outer_middleware(WelcomeMiddleware(bot, message_id))
+        
+        await send_message_and_delete(
+            bot=message.bot,
+            chat_id=message.chat.id,
+            text="👀<b>Success!</b> Rules have been successfully added!",
+        )
+    else:
+        await send_message_and_delete(
+            bot=message.bot,
+            chat_id=message.chat.id,
+            text="🔴Error: Please provide a valid message ID containing only digits.",
+            delay=10
+        )
+
+
 async def main():
-    """
-    Главная функция, запускающая бота
-    """
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        
+
     dp.message.middleware(AdminCheckerMiddleware(bot))
     dp.callback_query.middleware(CallbackAdminCheckerMiddleware(bot))
     dp.message.outer_middleware(ForbiddenWordsMiddleware(bot, forbidden_words))
