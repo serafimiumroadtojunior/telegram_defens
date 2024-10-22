f3801a411984bc84d61924abd16606090942d993
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-10-22 19:59:50 +0300

    Update warns_model.py

--- bot/database/models/warns_model.py
+++ bot/database/models/warns_model.py
@@ -1,17 +1,13 @@
-from typing import Annotated
-
 from sqlalchemy import BigInteger, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..session import Base
 
-primary_key = Annotated[int, mapped_column(primary_key=True)]
-warn_int = Annotated[int, mapped_column(Integer, default=0)]
 
 class Warns(Base):
     __tablename__ = "users_moderations"
     __table_args__ = {"schema" : "warns_system"}
 
-    id: Mapped[primary_key]
+    id: Mapped[int] = mapped_column(primary_key=True)
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True)
-    warns: Mapped[warn_int]
+    warns: Mapped[int] = mapped_column(Integer, default=0)
