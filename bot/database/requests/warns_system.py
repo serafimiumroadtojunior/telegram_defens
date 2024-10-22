4d57b14311ff7232b7bd41c2aedff5d5dcbc2a79
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-10-22 20:42:02 +0300

    Update warns_system.py

--- bot/database/requests/warns_system.py
+++ bot/database/requests/warns_system.py
@@ -20,14 +20,17 @@ async def add_user(tg_id: int) -> None:
 async def add_reason(tg_id: int, reason: str) -> None:
     async with async_session() as session:
         async with session.begin():
-            new_reason = Reasons(tg_id=tg_id, reasons=reason)
-            session.add(new_reason)
+            stmt = insert(Reasons).values(tg_id=tg_id, reasons=reason).on_conflict_do_nothing(
+                index_elements=['tg_id', 'reasons']
+            )
+            await session.execute(stmt)
 
 
 async def get_user_reasons(tg_id: int) -> Union[List[str], str]:
     async with async_session() as session:
         result: Result = await session.execute(
-            select(Reasons.reasons).where(Reasons.tg_id == tg_id)
+            select(Reasons.reasons)
+            .where(Reasons.tg_id == tg_id)
         )
 
         reasons: List[str] = [f"📝<b>Reason for warn: {reason}</b>" for reason in result.scalars()]
@@ -41,14 +44,18 @@ async def delete_user_reason(tg_id: int) -> None:
     async with async_session() as session:
         async with session.begin():
             subquery = select(Reasons.id).where(Reasons.tg_id == tg_id).order_by(Reasons.id.desc()).limit(1).scalar_subquery()
-            await session.execute(delete(Reasons).where(Reasons.id == subquery))
+            await session.execute(
+                delete(Reasons)
+                .where(Reasons.id == subquery)
+            )
 
 
 async def delete_user_reasons(tg_id: int) -> None:
     async with async_session() as session:
         async with session.begin():
             await session.execute(
-                delete(Reasons).where(Reasons.tg_id == tg_id) 
+                delete(Reasons)
+                .where(Reasons.tg_id == tg_id) 
             )
 
 
@@ -66,13 +73,18 @@ async def reset_warns(user_id: int) -> None:
     async with async_session() as session:
         async with session.begin():
             await session.execute(
-                update(Warns).where(Warns.tg_id == user_id).values(warns=0)
+                update(Warns)
+                .where(Warns.tg_id == user_id)
+                .values(warns=0)
             )
 
 
 async def check_warns(user_id: int) -> int:
     async with async_session() as session:
-        result: Result[tuple[int]] = await session.execute(select(Warns.warns).where(Warns.tg_id == user_id))
+        result: Result[tuple[int]] = await session.execute(
+            select(Warns.warns)
+            .where(Warns.tg_id == user_id)
+        )
 
         warns: Optional[int] = result.scalar_one_or_none()
         return warns if warns else 0
